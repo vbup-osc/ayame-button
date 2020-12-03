b9c53cb5f456460c1a347b9289142f2585e394f0
Voice Button United Studio <62581450+VoiceButtonUnitedProject@users.noreply.github.com>
2020-12-03 12:57:41 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@
         </div>
         <footer class="footer">
             <div class="container-fluid footer-content">
-                <button class="btn-buttom btn-buttom-info"><a href="javascript:location.reload();" style="color: #eeeeee;text-decoration: none">犬 1.0.0</a></button><button class="btn-buttom btn-buttom-info" style="margin-left:5px;"><a href="https://vercel.com?utm_source=oruyanke" style="color: #eeeeee;text-decoration: none"><img src="/resources/vercel.svg" style="height:13px;"></a></button>
+                <button class="btn-buttom btn-buttom-info"><a href="javascript:location.reload();" style="color: #eeeeee;text-decoration: none">犬 1.0.0</a></button><button class="btn-buttom btn-buttom-info" style="margin-left:5px;"><a href="https://www.vercel.com/?utm_source=vbuposc&utm_campaign=oss" style="color: #eeeeee;text-decoration: none"><img src="/resources/vercel.svg" style="height:13px;"></a></button>
                 <div class="pull-left">
                     <div class="text-left" style="color: #fd325a;">{{$t("info.author")}}</div>
                     <div class="text-left">{{$t("info.notOfficial")}}</div>
